c5fb6d132d6e22e971c54eda6489f7189b145366
Quan Thai <minhquan010710@gmail.com>
2026-09-08 17:00:33 +0700

    refactor: move work scripts

--- modules/dotfiles/default.nix
+++ modules/dotfiles/default.nix
@@ -4,9 +4,19 @@ let
   username = "quanthai";
   sourceRoot = ./home;
 
-  relativeFiles = map (path: lib.removePrefix "./" (lib.path.removePrefix sourceRoot path)) (
-    lib.filesystem.listFilesRecursive sourceRoot
-  );
+  discoveredFiles = map (
+    path: lib.removePrefix "./" (lib.path.removePrefix sourceRoot path)
+  ) (lib.filesystem.listFilesRecursive sourceRoot);
+
+  # Keep machine-local files out of Git while still linking them through Home Manager.
+  localFiles = [
+    ".config/kiro/settings"
+    ".config/sofka/clusters"
+    ".local/bin/notify-send"
+    ".zshenv_secret"
+  ];
+
+  relativeFiles = lib.unique (discoveredFiles ++ localFiles);
 in
 {
   home-manager.users.${username} =
--- modules/work/tecalliance/default.nix
+++ modules/work/tecalliance/default.nix
@@ -1,6 +1,32 @@
 { pkgs, ... }:
 
+let
+  dotfilesDir = builtins.getEnv "DOTFILES_DIR";
+  repoRoot = if dotfilesDir != "" then dotfilesDir else "/home/quanthai/personal/nixos";
+in
 {
+  home-manager.users.quanthai =
+    { config, ... }:
+    {
+      home.file = {
+        ".local/bin/tec_alarm" = {
+          force = true;
+          source = config.lib.file.mkOutOfStoreSymlink
+            "${repoRoot}/modules/work/tecalliance/scripts/tec_alarm";
+        };
+        ".local/bin/tec_deploy" = {
+          force = true;
+          source = config.lib.file.mkOutOfStoreSymlink
+            "${repoRoot}/modules/work/tecalliance/scripts/tec_deploy";
+        };
+        ".local/bin/tec_snapshot" = {
+          force = true;
+          source = config.lib.file.mkOutOfStoreSymlink
+            "${repoRoot}/modules/work/tecalliance/scripts/tec_snapshot";
+        };
+      };
+    };
+
   users.users.quanthai = {
     packages = with pkgs; [
       (azure-cli.withExtensions [
