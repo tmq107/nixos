948e711aceca25440e1894d948f9d0b0fe1ddea1
Quan Thai <minhquan010710@gmail.com>
2026-09-17 11:34:02 +0700

    fix: azure-devops mcp require some packages

--- modules/work/tecalliance/default.nix
+++ modules/work/tecalliance/default.nix
@@ -2,9 +2,38 @@
 
 let
   dotfilesDir = builtins.getEnv "DOTFILES_DIR";
-  repoRoot = if dotfilesDir != "" then dotfilesDir else "/home/quanthai/personal/nixos";
+  repoRoot =
+    if dotfilesDir != ""
+    then dotfilesDir
+    else "/home/quanthai/personal/nixos";
+
+  azureDevopsMcp = pkgs.writeShellScriptBin "azure-devops-mcp" ''
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
+      pkgs.libsecret
+      pkgs.glib
+    ]}"
+
+    exec ${pkgs.nodejs}/bin/npx \
+      -y @azure-devops/mcp "$@"
+  '';
 in
 {
+  users.users.quanthai = {
+    packages = with pkgs; [
+      (azure-cli.withExtensions [
+        azure-cli-extensions.azure-devops
+      ])
+
+      azureDevopsMcp
+
+      libsecret
+      glib
+      direnv
+      pre-commit
+      unstable.kiro-cli
+    ];
+  };
+
   home-manager.users.quanthai =
     { config, ... }:
     {
@@ -14,11 +43,13 @@ in
           source = config.lib.file.mkOutOfStoreSymlink
             "${repoRoot}/modules/work/tecalliance/scripts/tec_alarm";
         };
+
         ".local/bin/tec_deploy" = {
           force = true;
           source = config.lib.file.mkOutOfStoreSymlink
             "${repoRoot}/modules/work/tecalliance/scripts/tec_deploy";
         };
+
         ".local/bin/tec_snapshot" = {
           force = true;
           source = config.lib.file.mkOutOfStoreSymlink
@@ -26,15 +57,4 @@ in
         };
       };
     };
-
-  users.users.quanthai = {
-    packages = with pkgs; [
-      (azure-cli.withExtensions [
-        azure-cli-extensions.azure-devops
-      ])
-      direnv
-      pre-commit
-      unstable.kiro-cli
-    ];
-  };
 }
